9c481d91c22ed7744b73d1b8bc20b37f6925742f
wvanamstel <wlvanamstel@gmail.com>
2014-10-30 12:16:00 -0700

    first commit

--- app.py
+++ app.py
@@ -6,8 +6,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    lst = range(10)
-    return render_template('index.html', entries=lst)
+    test_dict = {'predictions': 0,
+                'approx_payout_date': 1279267200,
+                'body_length': 204,
+                'channels': 6,
+                'delivery_method': 0.0,
+                'event_created': 1278365733,
+                'event_end': 1278835200,
+                'event_published': 1278475736.0,
+                'event_start': 1278817200,
+                'fb_published': 0,
+                'gts': 515.0,
+                'has_analytics': 0,
+                'has_logo': 0,
+                'name_length': 21,
+                'num_order': 13,
+                'num_payouts': 0,
+                'org_facebook': 0.0,
+                'org_twitter': 0.0,
+                'sale_duration': 4.0,
+                'sale_duration2': 5,
+                'show_map': 1,
+                'user_age': 217,
+                'user_created': 1259613950} 
+    return render_template('index.html', entries=test_dict)
 
 # @app.route('/score', methods=['POST'])
 # def score():
