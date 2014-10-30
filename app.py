f50bdcadb13459e8ba73c002f9607eecd819be34
Jeffrey Tang <jeffrey.tang09@gmail.com>
2014-10-30 01:03:45 -0700

    front end templates

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, render_template
-from cPickle import pickle
-from model import getdata, myModel
+import cPickle as pickle
+#from model import getdata, myModel
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return
+    lst = range(10)
+    return render_template('index.html', entries=lst)
 
-@app.route('/score', methods=['POST'])
-def score():
-    data = request.json
+# @app.route('/score', methods=['POST'])
+# def score():
+#     data = request.json
 
-@app.route('/dashboard')
-def dashboard():
-    return html
-
-requests.post('http://10.0.1.23.5000/register',)
+# @app.route('/dashboard')
+# def dashboard():
+#     pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
